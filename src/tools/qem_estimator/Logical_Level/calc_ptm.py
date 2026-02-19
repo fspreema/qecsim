a45fbdc4bac7822d76739636795da6f561d49368
Fabian Spreemann <f.spreemann@fz-juelich.de>
2026-02-19 15:22:01 +0100

    Updated PTM Class to ahndle only non zero creation of ptm and adding test cases.

--- src/tools/qem_estimator/Logical_Level/calc_ptm.py
+++ src/tools/qem_estimator/Logical_Level/calc_ptm.py
@@ -8,6 +8,31 @@ __all__ = ["PTMCalculator"]
 
 
 class PTMCalculator:
+    BASIS_TO_PAULI = {
+        "X": ["X+", "X-"],
+        "Y": ["Y+", "Y-"],
+        "Z": ["Z0", "Z1"],
+    }
+
+    NON_ZERO_FLOWS = [
+        "II->II",
+        "XI->XX",
+        "IX->IX",
+        "XX->XI",
+        "ZI->ZI",
+        "IZ->ZZ",
+        "ZZ->IZ",
+        "ZX->ZX",
+        "YZ->XY",
+        "YX->YI",
+        "YI->YX",
+        "YY->XZ",
+        "IY->ZY",
+        "XY->YZ",
+        "XZ->YY",
+        "ZY->IY",
+    ]
+
     def __init__(
         self,
         circuits: PTMCircuits,
@@ -34,9 +59,9 @@ class PTMCalculator:
         self.samples = samples
         self.circuits = circuits.circuits
 
-    def calc_ptm(self):
+    def calc_ptm(self, only_non_zero: bool = False) -> np.ndarray:
         # Calculate the expectation values for each basis combination
-        exp_vals_per_basis_comb = self._calc_entries()
+        exp_vals_per_basis_comb = self._calc_entries_for_surgery(only_non_zero=only_non_zero)
 
         # Build the PTM Matrix out of the expectation values
         ptm_matrix = self._build_mtx_for_ptm(exp_vals_per_basis_comb)
@@ -62,7 +87,7 @@ class PTMCalculator:
         if n_qubits == 1:
             len_mtx = 3
         elif n_qubits == 2:
-            len_mtx = 15
+            len_mtx = 16
 
         # Initialize empty PTM Matrix
         ptm_matrix = np.zeros((len_mtx, len_mtx))
@@ -151,7 +176,73 @@ class PTMCalculator:
 
         return matcher, dem
 
-    def _calc_entries(self):
+    @staticmethod
+    def _get_sgn(curr_basis_combination: str) -> dict[str, int]:
+        """
+        For a given Basis determine if the current combination is an identity combination
+        or not i.e. if a sign flip is needed for the expectation value calculation or not
+        """
+
+        # Split sign
+        input_pauli = curr_basis_combination.split("->")[0]
+        control_basis, target_basis = input_pauli
+
+        # Convention used later: we want a SUM for identity and a DIFFERENCE for non-identity.
+        # term = a + sgn*b  ->  sgn = +1 for sum, sgn = -1 for difference.
+        return {
+            "control": 1 if control_basis == "I" else -1,
+            "target": 1 if target_basis == "I" else -1,
+        }
+
+    @staticmethod
+    def _split_label(init_state_label: str) -> tuple[str, str]:
+        """
+        Splits a label such as "X+,Z0" into its control and target state components
+        """
+
+        if "," in init_state_label:
+            parts = [p for p in init_state_label.split(",")]
+
+        else:
+            raise ValueError(
+                f"Invalid init state label: {init_state_label!r}. "
+                f"Expected format 'ControlState,TargetState'.",
+            )
+
+        return parts[0], parts[1]
+
+    @staticmethod
+    def _get_init_pairing_value(
+        pair: tuple[str, str],
+        average_logical_state: dict[str, int],
+    ) -> float:
+        """
+        Given a pair of two states (e.g. (1, 0))) this function returns the corresponding
+        measurement result
+        """
+
+        # Init parameters
+        state_order = {"X+": 0, "X-": 1, "Y+": 0, "Y-": 1, "Z0": 0, "Z1": 1}
+        control_state, target_state = pair
+
+        for curr_init_state_label, exp_val in average_logical_state.items():
+            curr_control_state, curr_target_state = PTMCalculator._split_label(
+                curr_init_state_label,
+            )
+
+            if (state_order[curr_control_state] == control_state) and (
+                state_order[curr_target_state] == target_state
+            ):
+                return exp_val
+
+        raise ValueError(
+            f"Could not find matching init state for pair: {pair}.",
+        )
+
+    def _calc_entries_for_surface_patch(self):
+        pass
+
+    def _calc_entries_for_surgery(self, only_non_zero: bool = False) -> dict[str, float]:
         """
         We can't directly use the DEM as we need the
         raw measurements to infer what logical state we have
@@ -164,43 +255,99 @@ class PTMCalculator:
         exp_vals_per_basis_comb: dict[str, float] = {}
 
         # Run through diagonal circuits
-        for basis_combination, circuit_and_meas_rec in self.circuits.items():
-            circuit, meas_rec = circuit_and_meas_rec
+        for basis_combination, circuit_dict_and_meas_recs in self.circuits.items():
+            if only_non_zero and basis_combination not in self.NON_ZERO_FLOWS:
+                continue
 
-            # Buid the normal measurement smaples and sample n shots
-            sampler = circuit.compile_sampler()
-            results_samples = sampler.sample(shots=self.samples)
+            circuit_dict, meas_rec = circuit_dict_and_meas_recs
 
-            # Getting the individual logical states after measurement (Vectorized)
-            logical_states_noisy = self._get_logical_meas_from_samples(
-                results_samples,
-                meas_rec,
+            # For each circuit we need to calculate the expectation value
+            # For Identity we take the sum of the states!
+            # For X Y Z we calculate measurement(+eigenstate)
+            # - measurement(-eigenstate) / total number of samples
+
+            ####################### EXAMPLE 1 ################################
+            #       IX                                                       #
+            #       IX+ -> Z0 X+ (0+)                                        #
+            #           -> Z1 X+ (1+)                                        #
+            #                                                                #
+            #       IX- -> Z0 X- (0-)                                        #
+            #           -> Z1 X- (1-)                                        #
+            #                                                                #
+            #   We calc at the end (0+ + 1+) - (0- + 1-) / total samples     #
+            ##################################################################
+
+            ####################### EXAMPLE 2 ################################
+            #       ZX                                                       #
+            #       ZX+ -> Z0 X+ (0+)                                        #
+            #           -> Z1 X+ (1+)                                        #
+            #                                                                #
+            #       ZX- -> Z0 X- (0-)                                        #
+            #           -> Z1 X- (1-)                                        #
+            #                                                                #
+            #   We calc at the end (0+ - 1+) - (0- - 1-) / total samples     #
+            ##################################################################
+
+            average_logical_state: dict[str, int] = {}
+
+            for curr_init_state_label, curr_circuit in circuit_dict.items():
+                # Buid the normal measurement smaples and sample n shots
+                sampler = curr_circuit.compile_sampler()
+                results_samples = sampler.sample(shots=self.samples)
+
+                # Getting the individual logical states after measurement (Vectorized)
+                logical_states_noisy = self._get_logical_meas_from_samples(
+                    results_samples,
+                    meas_rec,
+                )
+
+                # Build DEM and Matcher
+                matcher, dem = self._get_dem_and_matcher(circuit=curr_circuit)
+
+                # Converting the measurement sample into DEM sample and continue as usual
+                # with decoding
+                detectors_observable_states = curr_circuit.compile_m2d_converter().convert(
+                    measurements=results_samples,
+                    append_observables=True,
+                )
+                detectors = detectors_observable_states[:, : dem.num_detectors]
+
+                # Decode all sample round in one Batch decode
+                is_flipped_prediction = matcher.decode_batch(detectors)
+
+                # Calculate final logical states (Vectorized)
+                # This returns an array of +1/-1 values
+                final_logical_states = self._xor_meas_and_decoder(
+                    is_flipped_prediction,
+                    logical_states_noisy,
+                )
+
+                # Take the Average of the logical state over all samples
+                average_logical_state[curr_init_state_label] = np.average(final_logical_states)
+
+            # Calculate Sign for the current basis combination
+            sgn_dict = self._get_sgn(basis_combination)
+            target_sgn = sgn_dict["target"]
+            control_sgn = sgn_dict["control"]
+
+            # Get the individual Terms for construction
+            term1 = self._get_init_pairing_value(
+                pair=(0, 0),
+                average_logical_state=average_logical_state,
+            ) + target_sgn * self._get_init_pairing_value(
+                pair=(1, 0),
+                average_logical_state=average_logical_state,
+            )
+            term2 = self._get_init_pairing_value(
+                pair=(0, 1),
+                average_logical_state=average_logical_state,
+            ) + target_sgn * self._get_init_pairing_value(
+                pair=(1, 1),
+                average_logical_state=average_logical_state,
             )
 
-            # Build DEM and Matcher
-            matcher, dem = self._get_dem_and_matcher(circuit)
-
-            # Converting the measurement sample into DEM sample and continue as usual with decoding
-            detectors_observable_states = circuit.compile_m2d_converter().convert(
-                measurements=results_samples,
-                append_observables=True,
-            )
-            detectors = detectors_observable_states[:, : dem.num_detectors]
-
-            # Decode all sample round in one Batch decode
-            is_flipped_prediction = matcher.decode_batch(detectors)
-
-            # Calculate final logical states (Vectorized)
-            # This returns an array of +1/-1 values
-            final_logical_states = self._xor_meas_and_decoder(
-                is_flipped_prediction,
-                logical_states_noisy,
-            )
-
-            # Take the Average of the logical state over all samples
-            average_logical_state = np.average(final_logical_states)
-
-            # Save the average logical state for the current basis combination to build the PTM
-            exp_vals_per_basis_comb[basis_combination] = average_logical_state
+            # Get Exp. Value and add to final Value Dict
+            exp_val = term1 + control_sgn * term2
+            exp_vals_per_basis_comb[basis_combination] = exp_val
 
         return exp_vals_per_basis_comb
--- tests/tools/test_ptm.py
+++ tests/tools/test_ptm.py
@@ -1,5 +1,4 @@
-import numpy as np
-
+from src.codes.lattice_surgery.builder import SurgeryBuilder
 from src.core.data_models import PTMCircuits
 from src.tools.qem_estimator.logical_level.calc_ptm import PTMCalculator
 
@@ -29,19 +28,75 @@ def test_pauli_to_string():
     assert PTMCalculator._map_pauli_string_to_indices("ZZ") == 15
 
 
-def test_construct_ptm_matrix_surface():
-    # Init Dummy Expectation Values for both Lattice Surgery and Surface Code
-    exp_vals_surface = {
-        "X->X": 1.0,
-        "Y->Y": 1.0,
-        "Z->Z": 1.0,
-    }
+def test_get_sgn():
+    # Test for Identity Output
+    assert PTMCalculator._get_sgn("XI->XI") == {"control": -1, "target": 1}
+    assert PTMCalculator._get_sgn("IX->IX") == {"control": 1, "target": -1}
 
-    # Get PTM Matrices
-    ptm_calculator = PTMCalculator(PTMCircuits(circuits={}), samples=1_000)
-    ptm_matrix_surface = ptm_calculator._build_mtx_for_ptm(exp_vals_surface)
+    # Test for Non-Identity Output
+    assert PTMCalculator._get_sgn("XI->XX") == {"control": -1, "target": 1}
+    assert PTMCalculator._get_sgn("IX->ZX") == {"control": 1, "target": -1}
 
-    # Assert that the PTM Matrices are correct
-    expected_ptm_surface = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
 
-    assert np.array_equal(ptm_matrix_surface, expected_ptm_surface)
+def test_split_label():
+    # Test Number of State Init Combinations
+    assert PTMCalculator._split_label("X+,Z0") == ("X+", "Z0")
+    assert PTMCalculator._split_label("Y-,Z1") == ("Y-", "Z1")
+    assert PTMCalculator._split_label("Z+,X0") == ("Z+", "X0")
+
+
+def test_get_init_pairing_value():
+    # Create dummy logical estimate dictionary
+    logical_estimate_dict = {"X+,Z0": 0.8, "X+,Z1": 0.2, "X-,Z0": 0.3, "X-,Z1": 0.7}
+
+    # Get value for pairings
+    assert (
+        PTMCalculator._get_init_pairing_value(
+            pair=(0, 0),
+            average_logical_state=logical_estimate_dict,
+        )
+        == 0.8
+    )
+    assert (
+        PTMCalculator._get_init_pairing_value(
+            pair=(0, 1),
+            average_logical_state=logical_estimate_dict,
+        )
+        == 0.2
+    )
+    assert (
+        PTMCalculator._get_init_pairing_value(
+            pair=(1, 0),
+            average_logical_state=logical_estimate_dict,
+        )
+        == 0.3
+    )
+    assert (
+        PTMCalculator._get_init_pairing_value(
+            pair=(1, 1),
+            average_logical_state=logical_estimate_dict,
+        )
+        == 0.7
+    )
+
+
+def test_get_entires_for_surgery():
+    """
+    Test if the get entries for surgery is working as expted
+    -> Using flows which should have expectation value of 1
+    -> Test Flow IX -> IX
+    """
+
+    # Create Circuits for testing
+    circ_1 = SurgeryBuilder(
+        distance=3,
+        control_state_init="Z0",
+        target_state_init="X+",
+        control_measure_basis="Z",
+        target_measure_basis="X",
+    )
+
+    # Adding all Circuit to the Dict
+    circuits_surgery = PTMCircuits(circuits=[])
+
+    pass
